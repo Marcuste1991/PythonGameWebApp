020106c1aa74f4d3f90999f77244186f93ba1004
Christopher Hübner <c_huebner91@yahoo.de>
2018-05-06 09:52:05 +0200

    Mainwebsite with low default css and html code - Improve follows

--- envor_main.py
+++ envor_main.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+
 app = Flask(__name__)
 
+# index -> main site view
 @app.route("/")
 def index():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    return render_template('index.html')
+
+# home -> news and picture overview
+@app.route("/home/")
+def home():
+    return render_template('home.html')
+
+# about -> information of all people connected to this project
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
+    ### for local testing uncomment first app.run and comment the other
+    # app.run(host='0.0.0.0', port=80)
+
+    ### for deployment uncomment second app.run and comment the other
     app.run(host='0.0.0.0')
