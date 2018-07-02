70a58f6d2556909404d90d0f740bc22a95b39b3d
Christopher Hübner <c_huebner91@yahoo.de>
2018-07-03 00:17:40 +0200

    V4 New Version

--- 2dshooter_v4/config.py
+++ 2dshooter_v4/config.py
@@ -14,7 +14,7 @@ WIDTH = 1024  # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 200
 TITLE = "Hunting Terrorist"
-VERSION = "1.00.022.1702"
+VERSION = "1.00.031.2382"
 BGCOLOR = DARKGREY
 
 TILESIZE = 40
